5039fa416b732bf3ff86c1f4979dfcf1c0c548e6
Ivan <lvan.lshchenko19.01@gmail.com>
2024-02-13 18:39:51 +0200

    Update command start

--- bot/commands/start.py
+++ bot/commands/start.py
@@ -6,6 +6,6 @@ from bot.keyboards import start_menu
 async def start(message: types.Message):
     await message.answer(
         "Привіт!\nЯ бот для генерації зображень. Просто напиши мені, яке саме зображення тобі потрібно.\n\nHello!"
-        "\nI am an image generation bot. Just write to me and tell me what kind of image you need.",
+        "\nI am an image generation bot. Just tell me what kind of image you need.",
         reply_markup=start_menu
     )
